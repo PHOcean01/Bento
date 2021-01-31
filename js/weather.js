abf292d98dd8c72f365a606d9a1c81bb2b1025df
Geffrey de Winter <geff@nanohost.nl>
2021-01-31 02:33:42 +0100

    added api key

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '96ac5a02324ad7bccbad09fcc804bfd6';
 
 // Set Position function
 setPosition();
